df57beb4f3b4019048eca162c976c22ff89f819f
LhordeS <lhorde@msn.com>
2026-01-13 19:14:48 +0900

    Add while loop to cap number of games. Condition to log final scores.

--- script.js
+++ script.js
@@ -16,32 +16,31 @@ function getComputerChoice (){
 
 const getHumanChoice = function (){
     const humanChoice = window.prompt(`Your Play!
-        Write rock, paper, or scissors`);
+        Write rock, paper, or scissors`).toLowerCase();
     return humanChoice;
 }
-
 
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound (humanChoice, computerChoice){
-    if (humanChoice.toLowerCase() === "rock" && computerChoice === "scissors"){
+    if (humanChoice === "rock" && computerChoice === "scissors"){
         alert("You win! Rock beats scissors.");
         humanScore++;
-    } else if (humanChoice.toLowerCase() === "paper" && computerChoice === "rock"){
+    } else if (humanChoice === "paper" && computerChoice === "rock"){
         alert("You win! Paper beats rock.");
         humanScore++
-    } else if (humanChoice.toLowerCase() === "scissors" && computerChoice === "paper") {
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
         alert("You win! Scissors beats paper.");
         humanScore++
-    } else if (humanChoice.toLowerCase() === "rock" && computerChoice === "paper"){
+    } else if (humanChoice === "rock" && computerChoice === "paper"){
         alert("You lose! Paper beats rock.");
         computerScore++;
-    } else if (humanChoice.toLowerCase() === "paper" && computerChoice === "scissors"){
+    } else if (humanChoice === "paper" && computerChoice === "scissors"){
         alert("You lose! Scissors beats paper.");
         computerScore++
-    } else if (humanChoice.toLowerCase() === "scissors" && computerChoice === "rock"){
+    } else if (humanChoice === "scissors" && computerChoice === "rock"){
         alert("You lose! Rock beats scissors.");
         computerScore++
     } else {
@@ -51,17 +50,20 @@ function playRound (humanChoice, computerChoice){
         Computer Score: ${computerScore}
         Your Score: ${humanScore}`);
 
-    if (computerScore < 3 && humanScore < 3) {
-        // playRound(getHumanChoice(), getComputerChoice());
-    } else if (computerScore >= 3){
-        alert("Computer wins best out of three!");
-        return;
-    } else if (humanScore >= 3){
-        alert("You win best out of three!");
-        return;
-    }
-    return;
     }
     
+function playGame (){
+    while (humanScore < 3 && computerScore < 3){
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+    
+    if (humanScore === 3) {
+        alert("First to 3. You win the game!");
+    } else {
+        alert("First to 3. Computer wins the game!");
+    }
+    }
 
-playRound(getHumanChoice(), getComputerChoice());
+    playGame();
